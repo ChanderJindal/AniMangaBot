929ac449f17b4d0b4ff1f9689240d3daf1aabac0
cj-kid <chanderjindal2@gmail.com>
2022-02-10 21:09:45 +0530

    Discord commands imported

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
 from distutils.debug import DEBUG
 import discord
 import commands as C
+from discord.ext import commands
 
-client = discord.Client()
+'''
+New Base!
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+'''
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+#client = discord.Client()
+
+@bot.event
 async def on_ready():
-    print('We can begin the Crafting as {0.user}'.format(client))
+    print('We can begin the Crafting as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hello!  {ctx.author}')
+  
+@bot.command()
+async def echo(ctx, arg):
+    await ctx.send(arg)
 
-  #commnad from bot it self no reply
-  if message.author == client.user:
-      return
+@bot.command()
+async def anime(ctx):
+    await ctx.send(embed = C.EpisodeUpdate())
 
-  prefix = "$"
-
-  #await message.channel.send(prefix)
-  # check message , removing case-sensitivity , checking prefix
-  msg = message.content.lower()
-
-  if msg == "prefix":
-    await message.channel.send(prefix)
-    return
-
-  if msg.startswith(prefix):
-      #message name update
-      msg = msg[len(prefix):]
-      #await message.channel.send(msg)
-      msg = msg.strip()
-      #basic starting test
-      if msg.startswith('hello'):
-        await message.channel.send('Hello!')
-        return
-    
-      if msg.startswith("echo"):
-        await message.channel.send(message.content[len(prefix) + len("echo"):])
-        return
-    
-      if "anime" in msg:
-        await message.channel.send(embed = C.EpisodeUpdate())
-        return
-      
-      if "manga" in msg:
-        await message.channel.send(embed =C.MangaUpdate())
-        return
+@bot.command()
+async def manga(ctx):
+    await ctx.send(embed = C.MangaUpdate())
 
 import os
 
 tk = os.environ['TOKEN']
 
-client.run(tk)
+bot.run(tk)
