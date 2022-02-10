2eaebe4817df8cf4608e0bca0d2915fd600c1cff
cj-kid <chanderjindal2@gmail.com>
2022-02-10 14:51:09 +0530

    Added Backup and removed some extra .txt files

--- animanga.py
+++ animanga.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup as BS
 import requests as r
+import json
 #Be sure to Install BeautifulSoup and lxml lib
 def Get_Soup(link):
   return BS( r.get(link).text, features="lxml")
 
-def Anime():
+
+def Anime():#This is main one
   #Site_Link = "https://myanimelist.net/anime/235/Detective_Conan/episode" #- slow updates 
   Site_Link = "https://ww1.gogoanime2.org/anime/detective-conan"
   Base_Soup = Get_Soup(Site_Link)
@@ -31,24 +33,155 @@ def Anime():
   Link_Part_1 = "https://ww1.gogoanime2.org/"
   #This is given
 
-  Link_Full = Link_Part_1 + Link_Part_2
+  GogoLink = Link_Part_1 + Link_Part_2
 
   Ep_Number = Latest_Ep.find('div',class_ = "name").text
   #this returns like 'EP 201'
 
-  EP_Type = Latest_Ep.find('div',class_="cate").text
+  #EP_Type = Latest_Ep.find('div',class_="cate").text
   #to see if it's 'DUB' or 'SUB'
 
   AniMixPlay_Link = f'https://animixplay.to/v1/detective-conan/' + str(Ep_Number).lower().replace(" ","")
 
-  #statement = f'{Ep_Number}  {EP_Type}\nThis Episode is Available on\n{AniMixPlay_Link}\n{Link_Full}'
+  #statement = f'{Ep_Number}  {EP_Type}\nThis Episode is Available on\n{AniMixPlay_Link}\n{GogoLink}'
 
-  return Ep_Number , AniMixPlay_Link , Link_Full,EP_Type
+  return Ep_Number , AniMixPlay_Link , GogoLink
   #f'''
   #New Episode #{EpisodeNumber} :- {EpisodeName}
   #Link:- {AniMix_Link}
   #'''
-def Manga():
+
+def AnimeBackup():
+  Site_Link = "https://myanimelist.net/anime/235/Detective_Conan/episode"
+
+  Base_Soup = Get_Soup(Site_Link)
+  #Initial Page
+
+  EpRange = Base_Soup.find('div', class_ = "pagination ac")
+  #Getting the range
+
+  LatestOne = EpRange.find_all('a',class_ = "link")[-1]
+  #Picked the Last Range
+
+  Correct_Page_Link = LatestOne["href"]
+
+  Correct_Soup = Get_Soup(Correct_Page_Link)
+  #Onto the page with Latest Ep
+
+  LatestEp = Correct_Soup.find_all('a', class_="fl-l fw-b")[-1]
+  #got the tag with correct info
+
+  EpisodeName = LatestEp.text
+  EpisodeNumber = LatestEp["href"].split('/')[-1]
+
+  AniMix_Link = "https://animixplay.to/v1/detective-conan/ep" + str(EpisodeNumber)
+  Gogo_Link = "https://ww1.gogoanime2.org//watch/detective-conan/" + str(EpisodeNumber)
+  #print(f'''
+  #New Episode #{EpisodeNumber} :- {EpisodeName}
+  #Link:- {AniMix_Link}
+  #'''
+  #)
+  return EpisodeName , EpisodeNumber, AniMix_Link, Gogo_Link
+
+
+########################################################## Manga ##########################################################
+
+def MangaDex_Anime_ID_update():#DC ->Detective Conan 
+  name = "detective conan"#this is just for the format
+  name = name.replace(" ","-")
+  Api_link = f'https://api.mangadex.org/manga?title={name}'
+
+
+  json_data = r.get(Api_link).json()
+  if len(json_data['data']) == 0:
+    return False
+  Anime_ID = ""
+  for i in range(0,10):
+    if json_data['data'][i]['attributes']['title']['en'] == "Detective Conan":
+      print(str(json_data['data'][i]['attributes']['title']['en']))
+      #To make sure that it's the same one,
+      Anime_ID = json_data['data'][i]['id']
+      break
+  # Since I know, that Anime_ID is not supposed to Change
+  f = open("Anime_ID.txt","w+")
+  f.write(str(Anime_ID))
+  f.close()
+  return Anime_ID
+
+def Last_Chapter_file_Update(Chapter):
+    f = open("Last_Chapter.txt","w+")
+    f.write(Chapter)
+    f.close()
+    return
+
+def Last_Chapter_Update():
+  Site_Link = "https://www.readdetectiveconanarc.com/"
+
+  soup = Get_Soup(Site_Link)
+
+  Link = soup.find('a', class_ = "column has-text-centered button-last-chapter")
+  Link = str(Link["href"])
+  #I know it was initially a string, but it was in form of a slice, thus immutable
+  Link = Link[0:len(Link)-1]
+  Chapter = Link.split('-')[-1]
+  Last_Chapter_file_Update(Chapter)
+  return Chapter
+
+def Manga():# For MangaDex 
+  Chapter = 1087
+  try:#If we got the Last_Chapter Number good, otherwise call the function above and make API call and get it
+    f = open("Last_Chapter.txt","r")
+    Chapter = f.read()
+    f.close()
+  except:
+    Chapter = Last_Chapter_Update()
+
+  Chapter = int(Chapter) + 1 #incase an update has been made
+
+  Anime_ID = ""
+
+  try:#If Anime ID is in file cool, otherwise make an API call
+    f = open("Anime_ID.txt","r")
+    Anime_ID = f.read()
+    f.close()
+  except:
+    Anime_ID = MangaDex_Anime_ID_update()
+  
+  Manga_ID = ""
+  #print(Anime_ID,Chapter)
+  try:
+  #Got the one for the newest expected Chapter
+    link = f'https://api.mangadex.org/chapter?manga={Anime_ID}&chapter={str(Chapter)}&translatedLanguage[]=en'
+    #Passing chapter as a string is best
+    response = r.get(link)
+    json_data = response.json()
+    #print(link)
+    Manga_ID = json_data['data'][0]['id']
+
+    Last_Chapter_file_Update(Chapter=Chapter)
+    #Since now it's Chapter + 1
+
+  except:
+    #So, chapter +1 is now out yet, so back to previous one
+      #print("here1")
+      Chapter = Chapter - 1
+      link = f'https://api.mangadex.org/chapter?manga={Anime_ID}&chapter={str(Chapter)}&translatedLanguage[]=en'
+      response = r.get(link)
+      json_data = response.json()
+      #print(link)
+    #Since last chapter update function sent me this detail, this chapter exists for sure
+      Manga_ID = json_data['data'][0]['id']
+
+  
+  #^This is the main Thing!
+
+  PageNumber = 1 #This one is for sake of format only
+
+  Final_Link = f'https://mangadex.org/chapter/{Manga_ID}/{PageNumber}'
+  
+  return Chapter , Final_Link
+
+def Manga_Backup():
   Site_Link = "https://www.readdetectiveconanarc.com/"
 
   soup = Get_Soup(Site_Link)
@@ -58,24 +191,27 @@ def Manga():
   #I know it was initially a string, but it was in form of a slice, thus immutable
   Link = Link[0:len(Link)-1]
 
-  Edition = Link.split('-')[-1]
+  Chapter = Link.split('-')[-1]
 
   '''
   To get first Page of Manga, but it already comes with the manga link
   soup = Get_Soup(Link)
-  ImageLink = soup.find('img', alt="Read Detective Conan Chapter "+str(Edition)+" - Page 1 For Free In The Highest Quality")["data-lazy-src"]
+  ImageLink = soup.find('img', alt="Read Detective Conan Chapter "+str(Chapter)+" - Page 1 For Free In The Highest Quality")["data-lazy-src"]
   '''
 
-  return Edition , Link 
+  return Chapter , Link 
   #f'''
-  #New Manga #{Edition}
+  #New Manga #{Chapter}
   #Link:- {Link}
   #{ImageLink}
   #'''
 
+
+
 if __name__ == "__main__":
   print("Here")
   print(Anime())
+  #print(MangaDex_Anime_ID_update())
   print("Here")
   print(Manga())
   print("Here")
--- commands.py
+++ commands.py
@@ -1,68 +1,42 @@
 import animanga as AM
 
-def MakeEpFile(Num):
-    f = open("Latest_Ep.txt",mode = "w")
-    f.write(str(Num))
-    f.close()
-
-def MakeMangaFile(Num):
-    f = open("Latest_Manga.txt",mode = "w")
-    f.write(str(Num))
-    f.close()
-
 def EpisodeUpdate():
-    Current_Number , AniLink , GogoLink, EpType = AM.Anime()
     try:
-        f1= open("Latest_Ep.txt",mode='r')
-        Last_Number = f1.read()
-        f1.close()
-        if Last_Number == Current_Number:
-            return f'''No Updates Yet. Lastest Episode till now is:-
-            {Current_Number}  {EpType}
-            can be found at 
-            {AniLink}
-            {GogoLink}
-            '''
-        else:
-            MakeEpFile(Current_Number)
-            return f'''The Latest Episode is out!
-            {Current_Number}  {EpType}
-            can be found at 
-            {AniLink}
-            {GogoLink}
-            '''
-    except:
-        MakeEpFile(Current_Number)
-        return f'''The Current Latest Episode is:-
-        {Current_Number}  {EpType}
-        can be found at 
-        {AniLink}
-        {GogoLink}
+        EpNumber , AniMixLink , GogoLink = AM.Anime()
+        return f'''ww1.gogoanime2.org says, that
+        Episode#{EpNumber} is available at {GogoLink}
+        You can also find it at 
+        {AniMixLink}
         '''
+    except:
+        try:
+            EpisodeName , EpisodeNumber, AniMixLink, GogoLink = AM.AnimeBackup()
+            return f'''myanimelist.net Says
+            Episode#**{EpisodeNumber}** - ***{EpisodeName}*** is available!
+            You can find them at
+            {GogoLink}
+            {AniMixLink}
+            '''
+        except:
+            return f'The bot is unable to search any Links for Detecctive Conan Episodes, pls have a look at back end.'
 
 def MangaUpdate():
-    Current_Number , link = AM.Manga()
+
     try:
-        f1= open("Latest_Manga.txt",mode='r')
-        Last_Number = f1.read()
-        f1.close()
-        if Last_Number == Current_Number:
-            return f'''No Updates Yet. Lastest Manga till now is:-
-            Manga Edition#{Current_Number} 
-            can be found at {link}
-            '''
-        else:
-            MakeMangaFile(Current_Number)
-            return f'''The Latest Manga is out!
-            Manga Edition#{Current_Number}
-            can be found at {link}
-            '''
-    except:
-        MakeMangaFile(Current_Number)
-        return f'''The Current Latest Manga is:-
-        Manga Edition#{Current_Number}
-        can be found at {link}
+        Chapter , MangaDexLink = AM.Manga()
+        return f'''MangaDex has got Chapter#{Chapter}. Read it at 
+        {MangaDexLink}
         '''
+    except:
+        try:
+            Chapter , Link = AM.Manga_Backup()
+            return f'''**readdetectiveconanarc.com** has got Chapter#{Chapter}. Read it at
+            {Link}
+            '''
+        except:
+            return f'The bot is unable to search any Links for Detecctive Conan Mangas, pls have a look at back end.'
+
+    
 if __name__ == "__main__":
     print(EpisodeUpdate())
     print(MangaUpdate())
