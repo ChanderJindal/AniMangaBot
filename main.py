3119e503b9d96f4881b1cc8807db8f18a9ee8262
cj-kid <chanderjindal2@gmail.com>
2022-02-13 16:28:05 +0530

    test updated

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ async def tester():
     await channel.send("This command is not here.")
 '''
 
-
-async def schedule_daily_message():
+async def AutoUpdates():
   AVal = C.LatestAnimeEpisode()
   MVal = C.LatestMangaChapter()
   while True:
@@ -56,7 +55,9 @@ async def schedule_daily_message():
           channel = bot.get_channel(Manga_Channel)
           await channel.send(embed=Val)
 
-    await asyncio.sleep(3600) #sleeps for 1 hr
+async def schedule_daily_message():
+  AutoUpdates()
+  await asyncio.sleep(3600) #sleeps for 1 hr
 
 @bot.event
 async def on_ready():
@@ -72,13 +73,7 @@ async def on_ready():
 async def test(ctx): #this is just to see if the IF CONDITION is working
   channel = bot.get_channel(test_channel)
   await channel.send("This command is here.")
-  Val = C.Anime_Update_Check()#checks anime updates, it has all the info there
-  if Val != False:#if anything but False then pass
-    await channel.send(embed=Val)
-
-  Val = C.Manga_Update_Check()#same as above but for manga
-  if Val != False:
-    await channel.send(embed=Val)
+  AutoUpdates()
   await channel.send("This command is not here.") 
 
 @bot.command()
