ec247da1d1c5d3340f7c37fd0288acb9ff8a19a4
cj-kid <chanderjindal2@gmail.com>
2022-03-05 19:55:39 +0530

    ASYNCIO AA GAYA!

--- commands.py
+++ commands.py
@@ -1,11 +1,12 @@
 import animanga as AM
 import discord
+import asyncio
 
 ##################################### Anime & Manga Updates #####################################
 
 def EpisodeUpdate():
     try:
-        EpNumber , AniMixLink , GogoLink = AM.Anime()
+        EpNumber , AniMixLink , GogoLink = asyncio.run(AM.Anime())
         embedVar = discord.Embed(title="gogoanime.film's Anime Update", description=f'**Episode#{EpNumber} is available!**', color=5763719)#Green
         embedVar.add_field(name="gogoanime.film 's Link", value=f'{GogoLink}', inline=False)
         embedVar.add_field(name="animixplay.to 's Link", value=f'{AniMixLink}', inline=False)
@@ -14,7 +15,7 @@ def EpisodeUpdate():
         #return  f'''ww1.gogoanime2.org says, that Episode#{EpNumber} is available at {GogoLink} You can also find it at {AniMixLink} '''
     except:
         try:
-            EpisodeNumber, EpisodeName, AniMixLink, GogoLink = AM.AnimeBackup()
+            EpisodeNumber, EpisodeName, AniMixLink, GogoLink =  asyncio.run(AM.AnimeBackup())
             embedVar = discord.Embed(title="myanimelist.net's Anime Update", description=f'**Episode#{EpisodeNumber} - {EpisodeName} is available!**', color=16705372)#Yellow
             embedVar.add_field(name="gogoanime.film 's Link", value=f'{GogoLink}', inline=False)
             embedVar.add_field(name="animixplay.to 's Link", value=f'{AniMixLink}', inline=False)
@@ -28,7 +29,7 @@ def EpisodeUpdate():
 def MangaUpdate(flag = False):
 
     try:
-        Chapter , MangaDexLink ,GroupName , UploaderName, FrontPage,Name = AM.Manga()
+        Chapter , MangaDexLink ,GroupName , UploaderName, FrontPage,Name =  asyncio.run(AM.Manga())
         embedVar = discord.Embed(title="MangaDex.org's Manga Update", description=f'**Chapter#{Chapter} - {Name} is available!**', color=5763719)#Green
         embedVar.add_field(name="Group Name", value=f'{GroupName}', inline=True) #Group  & Uploader name are as per API Regulation
         embedVar.add_field(name="Uploader Name", value=f'{UploaderName}', inline=True)
@@ -41,21 +42,21 @@ def MangaUpdate(flag = False):
                 AnimeID = str(f.read())
                 f.close()
             except:
-                AnimeID = AM.MangaDex_Anime_ID_update()
+                AnimeID =  asyncio.run(AM.MangaDex_Anime_ID_update())
             embedVar.set_image(url=AM.GetCover(AnimeID=AnimeID))
         return embedVar
         #return f'''MangaDex has got Chapter#{Chapter}. Read it at {MangaDexLink} '''
     except:
         try:
-            Chapter , Link = AM.Manga_Backup()
+            Chapter , Link =  asyncio.run(AM.Manga_Backup())
             embedVar = discord.Embed(title="readdetectiveconanarc.com's Manga Update", description=f'**Chapter#{Chapter} is available!**', color=16705372)#Yellow
             embedVar.add_field(name="readdetectiveconanarc.com 's Link", value=f'{Link}', inline=False)
             return embedVar
             #return f'''**readdetectiveconanarc.com** has got Chapter#{Chapter}. Read it at {Link} '''
         except:
             if (flag == False):
-                AM.MangaDex_Anime_ID_update()
-                AM.Last_Chapter_Update()
+                asyncio.run(AM.MangaDex_Anime_ID_update())
+                asyncio.run(AM.Last_Chapter_Update())
                 return MangaUpdate(True)
             embedVar = discord.Embed(title="Error!", description=f'**Bot is Unable to Detect any Detective Conan Manga!\nLook at the back-end.**', color=15548997)#Red
             return embedVar
@@ -91,7 +92,7 @@ def GetMangaChapterNumber(MyStr):
 if __name__ == "__main__":
     print(EpisodeUpdate())
     print(MangaUpdate())
-    print(AM.Anime())
-    print(AM.AnimeBackup())
-    print(AM.Manga())
-    print(AM.Manga_Backup())
+    print(asyncio.run(AM.Anime()))
+    print(asyncio.run(AM.AnimeBackup()))
+    print(asyncio.run(AM.Manga()))
+    print(asyncio.run(AM.Manga_Backup()))
