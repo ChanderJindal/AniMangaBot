449a862e3988d46d52488710a7364bf135cdc118
cj-kid <chanderjindal2@gmail.com>
2022-03-06 01:30:59 +0530

    removed few unused lines

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from time import sleep
 import discord
 import commands as C
 from discord.ext import commands
-import asyncio
 import helper_commands as hp
 
 Yeah = ["y","ye","yes","Okay","k","kay","true","t","true",'enable', 'on']
