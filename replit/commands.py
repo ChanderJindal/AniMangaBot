c40b629fd0c8d0a3d0c7b53021c4d8bdcacf115b
Chander Jindal <72246687+ChanderJindal@users.noreply.github.com>
2022-04-07 20:45:36 +0530

    Update commands.py

--- replit/commands.py
+++ replit/commands.py
@@ -5,24 +5,43 @@ from replit import db
 
 ##################################### Anime & Manga Updates #####################################
 
+async def EpisodeUpdate0():
+  try:
+      EpNumber , AniMixLink , GogoLink,Update = await AM.Anime0()
+      embedVar = discord.Embed(title="Zero's Tea Time.")
+      embedVar.add_field(name="gogoanime.film ", value="Update", inline=True)
+      embedVar.description=f'**Episode#{EpNumber} is available!**'
+      embedVar.color=5763719#Green
+      embedVar.add_field(name="gogoanime.film 's Link", value=f'{GogoLink}', inline=False)
+      embedVar.add_field(name="animixplay.to 's Link", value=f'{AniMixLink}', inline=False)
+      embedVar.set_image(url="https://gogocdn.net/cover/meitantei-conan-zero-no-tea-time.png")
+      #await message.channel.send(embed=embedVar)
+      print("HereEP0",EpNumber,Update,sep=" - ")
+      return embedVar,Update
+      #return  f'''ww1.gogoanime2.org says, that Episode#{EpNumber} is available at {GogoLink} You can also find it at {AniMixLink} '''
+  except:
+    embedVar = discord.Embed(title="Error!", description=f'**Bot is Unable to Detect any Detective Conan Episodes!\nLook at the back-end.**', color=15548997)#Red
+    return embedVar
+
 async def EpisodeUpdate():
   try:
-      EpNumber , AniMixLink , GogoLink = await AM.Anime()
+      EpNumber , AniMixLink , GogoLink,Update = await AM.Anime()
       embedVar = discord.Embed(title="gogoanime.film's Anime Update", description=f'**Episode#{EpNumber} is available!**', color=5763719)#Green
+      embedVar.set_image(url="https://gogocdn.net/cover/detective-conan.png")
       embedVar.add_field(name="gogoanime.film 's Link", value=f'{GogoLink}', inline=False)
       embedVar.add_field(name="animixplay.to 's Link", value=f'{AniMixLink}', inline=False)
       #await message.channel.send(embed=embedVar)
-      print("HereEP1")
-      return embedVar
+      print("HereEP1",EpNumber,Update,sep=" - ")
+      return embedVar,Update
       #return  f'''ww1.gogoanime2.org says, that Episode#{EpNumber} is available at {GogoLink} You can also find it at {AniMixLink} '''
   except:
       try:
-          EpisodeNumber, EpisodeName, AniMixLink, GogoLink =  await AM.AnimeBackup()
+          EpisodeNumber, EpisodeName, AniMixLink, GogoLink,Update =  await AM.AnimeBackup()
           embedVar = discord.Embed(title="myanimelist.net's Anime Update", description=f'**Episode#{EpisodeNumber} - {EpisodeName} is available!**', color=16705372)#Yellow
           embedVar.add_field(name="gogoanime.film 's Link", value=f'{GogoLink}', inline=False)
           embedVar.add_field(name="animixplay.to 's Link", value=f'{AniMixLink}', inline=False)
-          print("HereEP2")
-          return embedVar
+          print("HereEP2",EpNumber,Update,sep=" - ")
+          return embedVar,Update
           #return f'''myanimelist.net Says Episode#**{EpisodeNumber}** - ***{EpisodeName}*** is available! You can find them at {GogoLink} {AniMixLink} '''
       except:
           embedVar = discord.Embed(title="Error!", description=f'**Bot is Unable to Detect any Detective Conan Episodes!\nLook at the back-end.**', color=15548997)#Red
@@ -33,7 +52,7 @@ async def EpisodeUpdate():
 async def MangaUpdate(flag = False):
 
   try:
-      Chapter , MangaDexLink ,GroupName , UploaderName, FrontPage,Name =  await AM.Manga()
+      Chapter , MangaDexLink ,GroupName , UploaderName, FrontPage,Name,Update =  await AM.Manga()
       embedVar = discord.Embed(title="MangaDex.org's Manga Update", description=f'**Chapter#{Chapter} - {Name} is available!**', color=5763719)#Green
       embedVar.add_field(name="Group Name", value=f'{GroupName}', inline=True) #Group  & Uploader name are as per API Regulation
       embedVar.add_field(name="Uploader Name", value=f'{UploaderName}', inline=True)
@@ -46,16 +65,16 @@ async def MangaUpdate(flag = False):
           except:
               AnimeID = db["MangaDexAnimeID"]
               embedVar.set_image( url = AM.GetCover( AnimeID = AnimeID ) )
-      print("HereM1")
-      return embedVar
+      print("HereM1",Chapter,Update,sep=" - ")
+      return embedVar,Update
       #return f'''MangaDex has got Chapter#{Chapter}. Read it at {MangaDexLink} '''
   except:
       try:
-          Chapter , Link =  await AM.Manga_Backup()
+          Chapter , Link,Update =  await AM.Manga_Backup()
           embedVar = discord.Embed(title="readdetectiveconanarc.com's Manga Update", description=f'**Chapter#{Chapter} is available!**', color=16705372)#Yellow
           embedVar.add_field(name="readdetectiveconanarc.com 's Link", value=f'{Link}', inline=False)
           print("HereM2")
-          return embedVar
+          return embedVar,Update
           #return f'''**readdetectiveconanarc.com** has got Chapter#{Chapter}. Read it at {Link} '''
       except:
           if (flag == False):
@@ -79,7 +98,6 @@ def GetMangaChapterNumber(MyStr):
     lst = MyStr.split(" ")
     MyStr = lst[0][len("**Chapter#"):]
     return int(MyStr)
-
 
     
 if __name__ == "__main__":
