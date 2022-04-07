5496852c5cf03c1c3cb5657e2f3206a21e4a947f
Chander Jindal <72246687+ChanderJindal@users.noreply.github.com>
2022-04-07 20:45:20 +0530

    Update animanga.py

--- replit/animanga.py
+++ replit/animanga.py
@@ -4,10 +4,6 @@ import aiohttp
 import asyncio
 from replit import db
 
-db["Ep"] = 1037
-db["MangaDexAnimeID"] = "7f30dfc3-0b80-4dcc-a3b9-0cd746fac005"
-db["Chapter"] = 1088
-
 async def Get_Soup( URL : str , format = "html" ):
     async with aiohttp.ClientSession() as session:
         async with session.get(URL) as response:
@@ -30,11 +26,40 @@ async def Anime():
   GogoLink = Link_base + EpNumber.replace(".","-")
 
   AniMixPlay_Link = f'https://animixplay.to/v1/detective-conan/ep{EpNumber}'
+  Update = False
+  if str(db["Ep"]) != str(EpNumber):
+    Update = True
 
   db["Ep"] = EpNumber
 
-  return EpNumber , AniMixPlay_Link , GogoLink
+  return EpNumber , AniMixPlay_Link , GogoLink , Update
 
+async def Anime0():
+  #print(db["Ep0"])
+  Site_Link = "https://gogoanime.gg/category/meitantei-conan-zero-no-tea-time"
+  Base_Soup = await Get_Soup(Site_Link)
+
+  Main_div = Base_Soup.find('div',class_ = "anime_video_body")
+
+  Sub_div = Main_div.find_all('li')[-1]
+
+  EpNumber = Sub_div.a['ep_end']
+
+  Link_base = "https://gogoanime.film/detective-conan-episode-"
+
+  GogoLink = Link_base + EpNumber.replace(".","-")
+
+  AniMixPlay_Link = f'https://animixplay.to/v1/detective-conan/ep{EpNumber}'
+
+  db["Ep0"] = EpNumber
+  
+  Update = False
+  if str(db["Ep0"]) != str(EpNumber):
+    Update = True
+
+  db["Ep0"] = EpNumber
+
+  return EpNumber , AniMixPlay_Link , GogoLink , Update
 
 async def AnimeBackup():
   Site_Link = "https://myanimelist.net/anime/235/Detective_Conan/episode"
@@ -56,10 +81,14 @@ async def AnimeBackup():
 
   AniMix_Link = f'https://animixplay.to/v1/detective-conan/ep{EpisodeNumber}'
   Gogo_Link = f'https://gogoanime.film/detective-conan-episode-{EpisodeNumber}'
-
+  
+  Update = False
+  if str(db["Ep"]) != str(EpisodeNumber):
+    Update = True
+    
   db["Ep"] = EpisodeNumber
 
-  return EpisodeNumber, EpisodeName ,AniMix_Link, Gogo_Link
+  return EpisodeNumber, EpisodeName ,AniMix_Link, Gogo_Link,Update
 
 ###########################################
 
@@ -120,6 +149,7 @@ async def Manga():# For MangaDex
   GroupName = ""
   UploaderName = ""
   Name = ""
+  Update = False
   try:
 
     link = f'https://api.mangadex.org/chapter?manga={Anime_ID}&chapter={str(Chapter)}&translatedLanguage[]=en'
@@ -129,7 +159,7 @@ async def Manga():# For MangaDex
     Manga_ID = json_data['data'][0]['id']
     GroupName , UploaderName = await GroupUploader(json_data=json_data)
     Name = json_data['data'][0]['attributes']['title']
-    db["Chapter"] = Chapter
+    Update = True
 
   except:
       Chapter = int(Chapter) - 1
@@ -141,11 +171,12 @@ async def Manga():# For MangaDex
       Name = json_data['data'][0]['attributes']['title']
   
   PageNumber = 1 
+  db["Chapter"] = Chapter
 
   Final_Link = f'https://mangadex.org/chapter/{Manga_ID}/{PageNumber}'
   ImageLink = await GetFrontPage(MangaID=Manga_ID)
   
-  return str(Chapter) , Final_Link , GroupName , UploaderName,ImageLink,Name
+  return str(Chapter) , Final_Link , GroupName , UploaderName,ImageLink,Name, Update
 
 async def GetFrontPage(MangaID):
   BaseLink = f"https://api.mangadex.org/at-home/server/{MangaID}"
@@ -192,22 +223,41 @@ async def Manga_Backup():
 
   Chapter = Link.split('-')[-1]
 
-  return Chapter , Link 
+  Update = False
+  if str(db["Chapter"]) != str(Chapter):
+    Update = True
+    db["Chapter"] = Chapter
+
+  return Chapter , Link , Update
   #f'''
   #New Manga #{Chapter}
   #Link:- {Link}
   #{ImageLink}
   #'''
 
+db_keys = db.keys()
+if "Ep" not in db_keys:
+  db["Ep"] = 1037
+  #asyncio.run(await Anime())
+if "MangaDexAnimeID" not in db_keys:
+  db["MangaDexAnimeID"] = "7f30dfc3-0b80-4dcc-a3b9-0cd746fac005"
+  #asyncio.run(await MangaDex_Anime_ID_update())
+if "Chapter" not in db_keys:
+  db["Chapter"] = 1089
+  #asyncio.run(await Manga_Backup())
+
+if "Ep0" not in db_keys:
+  db["Ep0"] = 0
+
 if __name__ == "__main__":
-    a,b,c=asyncio.run(Anime())
-    print(a,b,c,sep='\n',end="\n#####\n")
-    a,b,c,d=asyncio.run(AnimeBackup())
+    a,b,c,d=asyncio.run(Anime())
     print(a,b,c,d,sep='\n',end="\n#####\n")
-    a,b,c,d,e,f = asyncio.run(Manga())
-    print(a,b,c,d,e,f,sep='\n',end="\n#####\n")
-    a,b=asyncio.run(Manga_Backup())
-    print(a,b,sep='\n',end="\n#####\n")
+    a,b,c,d,e=asyncio.run(AnimeBackup())
+    print(a,b,c,d,e,sep='\n',end="\n#####\n")
+    a,b,c,d,e,f,g = asyncio.run(Manga())
+    print(a,b,c,d,e,f,g,sep='\n',end="\n#####\n")
+    a,b,c=asyncio.run(Manga_Backup())
+    print(a,b,c,sep='\n',end="\n#####\n")
 
 '''
 #Director's Commentary
